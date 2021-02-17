1db96cd27aa3f8a9b00a1ae68e6cb7387ddefe0f
Edilberto Zelaya <edilberto.zelayabarcenas@laureate.net>
2021-02-16 18:47:44 -0600

    Added ListUsers View

--- CertificatesApp/app.js
+++ CertificatesApp/app.js
@@ -1,8 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
+var exphbs = require('express-handlebars')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,11 +14,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+/* app.use(express.json());
+app.use(express.urlencoded({ extended: false })); */
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
