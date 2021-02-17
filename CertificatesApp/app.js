834cb423cea25d976ca95591d064ba9c8ea443b6
Edilberto Zelaya <edilberto.zelayabarcenas@laureate.net>
2021-02-16 19:28:04 -0600

    Added createUser view

--- CertificatesApp/app.js
+++ CertificatesApp/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var createUserRouter = require('./routes/createUser')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/createUser', createUserRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ CertificatesApp/models/user.js
@@ -0,0 +1,6 @@
+class User {
+    constructor (userName, userPassword){
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+}
