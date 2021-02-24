46cad2128ca857c9c9855a4f185f590e88107eb3
Edilberto Zelaya <edilberto.zelayabarcenas@laureate.net>
2021-02-24 14:43:13 -0600

    Added loginController

---
+++ CertificatesApp/controllers/loginController.js
@@ -0,0 +1,18 @@
+module.exports.verifyCredentiales = (db, user) => {
+    return new Promise((res, rej) =>{
+        let psw = Buffer.from(user.userPassword, 'base64').toString();
+        sha1 = require('js-sha1');
+        let password = sha1(psw)
+        db.executeQuery("SELECT UserName FROM dbo.[User] WHERE UserName='"+user.userName+"' AND UserPassword='"+password+"'")
+        .then(function(response){
+            if(response[0]==undefined) {
+                res(false)
+            } else {
+                res(response[0].UserName)
+            }
+        })
+        .catch(function(err){
+            rej(err);
+        })
+    })
+}
